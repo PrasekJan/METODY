4a1f46aaf329d781a7a49208ee53ea6d08064090
Prášek <Prek@DESKTOP-89RHD4J>
2021-03-08 13:36:14 +0100

    Metody06

--- METODY/Form1.cs
+++ METODY/Form1.cs
@@ -16,5 +16,22 @@ namespace METODY
         {
             InitializeComponent();
         }
+        int CifLichSoucet(int a)
+        {
+            int soucet = 0;
+            while (a > 0)
+            {
+                if ((a % 10) % 2 != 0) soucet += a % 10;
+                a /= 10;
+            }
+            return soucet;
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+                int a = Convert.ToInt32(textBox1.Text);
+                label1.Text = CifLichSoucet(a).ToString();
+
+        }
     }
 }
